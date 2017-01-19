7d1e6602ba0a7d526e214e250fe9f3707a073c6f
Allen Roberts <allen.roberts1985@gmail.com>
2017-01-19 16:43:30 -0500

    CommandConsole first push

---
+++ CommandConsole/ConsoleBuffer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CommandConsole
+{
+    public class ConsoleBuffer
+    {
+        /// <summary>
+        /// Update console delegate
+        /// </summary>
+        public Action BufferUpdated;
+        /// <summary>
+        /// Output buffer
+        /// </summary>
+        private static List<StringData> _obuffer = new List<StringData>();
+        /// <summary>
+        /// Public output buffer. Used to invoke BufferUpdated when updated.
+        /// </summary>
+        public List<StringData> OutputBuffer { get { return _obuffer; } private set { _obuffer = value; } }
+
+        public ConsoleBuffer()
+        {
+        }
+
+        /// <summary>
+        /// Write to OutputBuffer as an error
+        /// </summary>
+        /// <param name="t">text to write</param>
+        public void Error(string t)
+        {
+            WriteColor("!!! " + t, new SolidColorBrush(Colors.DarkRed));
+        }
+        /// <summary>
+        /// Write to OutputBuffer as Help text
+        /// </summary>
+        /// <param name="t">Text to write</param>
+        public void Help(string t)
+        {
+            WriteColor("??? " + t, new SolidColorBrush(Colors.Green));
+        }
+        /// <summary>
+        /// Write to OutputBuffer as standard info.
+        /// </summary>
+        /// <param name="t">Text to write.</param>
+        public void Write(string t)
+        {
+            WriteColor(t, new SolidColorBrush(Colors.Black));
+        }
+        /// <summary>
+        /// Write to console as a specific color
+        /// </summary>
+        /// <param name="t">Text</param>
+        /// <param name="b">Color</param>
+        public void WriteColor(string t, Brush b)
+        {
+            OutputBuffer.Add(new StringData(b, t));
+            BufferUpdated?.Invoke();
+        }
+        /// <summary>
+        /// Holds string data
+        /// </summary>
+        public struct StringData
+        {
+            /// <summary>
+            /// Color of the string
+            /// </summary>
+            public Brush DisplayColor { get; set; }
+            /// <summary>
+            /// Text to be displayed
+            /// </summary>
+            public string Text { get; set; }
+            /// <summary>
+            /// Generate a new set of StringData
+            /// </summary>
+            /// <param name="cc">string color</param>
+            /// <param name="t">text info</param>
+            public StringData(Brush cc, string t) { Text = t;  DisplayColor = cc; }
+        }
+    }
+}
---
+++ CommandConsole/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandConsole
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConsoleBuffer buff = new ConsoleBuffer();
+        string InputToken => ">> ";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            buff.BufferUpdated = WriteBuffer;
+
+            //setup ConsoleInput textbox.
+            ConsoleInput.Text = InputToken;
+            ConsoleInput.CaretBrush = new SolidColorBrush(Colors.Black);
+
+            //focus ConsoleInput and stay there
+            ConsoleInput.Focus();
+            ConsoleInput.LostKeyboardFocus += (object o, KeyboardFocusChangedEventArgs e) => ConsoleInput.Focus();
+
+            //Launch text
+            buff.Write("COMMAND CONSOLE\n\nCreator: Allen Roberts\nemail: allen.roberts1985@gmail.com\ngit: github.com/arobert2");
+        }
+
+        public void WriteBuffer()
+        {
+            TextRange tr = new TextRange(ConsoleOutput.Document.ContentEnd, ConsoleOutput.Document.ContentEnd);
+            tr.Text = buff.OutputBuffer[buff.OutputBuffer.Count - 1].Text + "\n";
+            tr.ApplyPropertyValue(TextElement.ForegroundProperty, buff.OutputBuffer[buff.OutputBuffer.Count - 1].DisplayColor);
+        }
+
+        public void CommandInputed()
+        {
+            ConsoleInput.Text = InputToken;
+        }
+
+        public void OnEnter()
+        {
+            string CommandString = ConsoleInput.Text.Substring(3).ToLower();
+        }
+
+        public class CBuffer
+        {
+            public static ConsoleBuffer Instance { get; set; }
+
+            public CBuffer()
+            {
+                Instance = Instance == null ? new ConsoleBuffer() : Instance;
+            }
+        }
+    }
+}
