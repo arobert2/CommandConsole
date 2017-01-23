76ce4e8f51a9cfb27731a5be6f6473fbe2a599a4
Allen.Roberts <allen.roberts1985@gmail.com>
2017-01-22 22:42:00 -0500

    static task system for cordinating threads and user interaction.

---
+++ CommandConsole/TaskSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CommandConsole
+{
+    public static class TaskSystem
+    {
+        /// <summary>
+        /// Task currently being manipulated.
+        /// </summary>
+        public static Stack<string> ActiveTask { get; set; } = new Stack<string>();
+        /// <summary>
+        /// CurrentLibrary
+        /// </summary>
+        public static CommandLibrary CurrentLibrary { get; set; }
+        /// <summary>
+        /// Current running tasks.
+        /// </summary>
+        public static Dictionary<string, Task> RunningTasks = new Dictionary<string, Task>();
+        /// <summary>
+        /// Command Queue.
+        /// </summary>
+        public static Dictionary<string, Queue<string>> CommandQueue = new Dictionary<string, Queue<string>>();
+        /// <summary>
+        /// Enter a command.
+        /// </summary>
+        /// <param name="c"></param>
+        public static void EnterCommand(string c)
+        {
+            CommandQueue[ActiveTask.Peek()].Enqueue(c);
+        }
+    }
+}
